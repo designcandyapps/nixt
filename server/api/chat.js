9ad09531e31de8ba48b8e6f66728cd377b00f9b3
Erin Dunn Halligan <erin@designcandy.com>
2025-02-11 07:50:20 -0600

    Update chat.js

--- server/api/chat.js
+++ server/api/chat.js
@@ -6,7 +6,7 @@ export default defineEventHandler(async(e)=>{
       "Content-Type":"application/json",
       Authorization:`Bearer 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`,
     },
-    body:JSON.stringify({model:"gpt-3.5-turbo",messages:[{role:"user",co:b.ms}]}),
+    b:JSON.stringify({model:"gpt-3.5-turbo",messages:[{role:"user",co:b.ms}]}),
   });
   const d=await ar.json(); return{re:d.choices[0].ms.co};
 });
