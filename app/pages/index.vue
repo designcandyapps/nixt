6bf8f43b4dbd9268005b246ebe4dbfb1340df5f6
Erin Dunn Halligan <erin@designcandy.com>
2025-02-11 17:11:12 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,7 +6,7 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 <template>
   <div>
     <ULandingHero v-if="page.hero" v-bind="page.hero">
-      <canvas id="ca" style="border:1px solid pink;"></canvas>
+      <img id="ii" src="https://pinfluents.com/im/lo.png"><canvas id="ca" style="border:1px solid pink;"></canvas>
       <div class="g"><input id="q" v-model="q"><div id="response" v-if="response">{{response}}</div><ImageGenerator /></div>
       <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
     </ULandingHero><ULandingSection :title="page.features.title" :links="page.features.links"><UPageGrid><ULandingCard v-for="(item,index) of page.features.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
@@ -14,18 +14,36 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 </template>
 
 <script lang="ts">
-import gen from '~/components/gen.vue';
 export default{
-  components:{gen},
-  data(){return{q:"",response:null}},
-  mounted(){setTimeout(()=>{this.snd()},1600)},
+  //data(){return{q:"",response:null}},
+  mounted(){this.loadColor()},
   methods:{
-    async snd(){
-      const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector('#q').value})});
-      const data=await response.json(); this.response=data.reply;
-      //alert("R1: "+this.response);
-      document.querySelector('#t').innerText=this.response;
-    },
+  async itB(ii){
+    const ca=document.getElementById("ca");
+    ca.width=ii.width; ca.height=ii.height;
+    const cx=ca.getContext("2d");
+    cx.drawImage(ii,0,0);
+    alert("CA: "+ca);
+
+    return ca.toDataURL();
+    alert("Test");
   },
+  async loadColor(){
+    const f=document.getElementById("ii");
+    const r=new FileReader();
+    r.onload=function(e){
+      const im=new Image();
+      const img=document.getElementById("ii");
+      const bs=itB(img); im.src=bs;
+      alert("IMSRC2: "+im.src);
+
+      im.onload=()=>{
+       alert("IM2: "+im.src);
+
+      }
+    }
+    const j=JSON.stringify(f); f=new Blob([j],{type:"application/image"}); r.readAsDataURL(f);
+  }
+  }
 }
 </script>
