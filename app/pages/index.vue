d3719032c687432d470352fb966ec0c8a239ef46
Erin Dunn Halligan <erin@designcandy.com>
2025-02-10 21:59:00 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -14,35 +14,5 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 </template>
 
 <script lang="ts">
-function imageToBase64(ii){
-  const ca=document.createElement("canvas");
-  ca.width=ii.width; ca.height=ii.height;
-  const cx=ca.getContext("2d"); cx.drawImage(ii,0,0);
-  return ca.toDataURL();
-}
-const f=document.querySelector("#ii");
-const r=new FileReader();
-r.onload=function(e){
-  const im=new Image();
-  const img=document.querySelector("ii");
-  const bs=imageToBase64(img); im.src=bs;
-  im.onload=function(){
-    const ca=document.createElement("canvas");
-    ca.width=im.width; ca.height=im.height;
-    const cx=ca.getContext("2d");
-    cx.drawImage(im,0,0);
-    const o=cx.getImageData(0,0,ca.width,ca.height);
-    const d=o.data; const cc={}; let mc=0; let dc="";
-    for(let i=0;i<d.length;i+=4){
-      const r=d[i]; const g=d[i+1]; const b=d[i+2];
-      const rgb=`${r},${g},${b}`;
-      if(cc[rgb]){cc[rgb]++}else{cc[rgb]=1}
-      if(cc[rgb]>mc){mc=cc[rgb]; dc=rgb}
-    }
-    document.body.style.backgroundColor=`rgb(${dc})`;
-  }
-}
-const j=JSON.stringify(f);
-f=new Blob([j],{type:"application/image"});
-r.readAsDataURL(f);
+
 </script>
