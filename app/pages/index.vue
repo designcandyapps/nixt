fd65b588a3d18d0320df193bac32bbd44dbb22f2
Erin Dunn Halligan <erin@designcandy.com>
2025-02-11 08:15:37 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,7 +6,7 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 <template>
   <div>
     <ULandingHero v-if="page.hero" v-bind="page.hero">
-      <div class="g"><input id="q" v-model="q"><div id="v" v-if="v">{{v}}</div><ImageGenerator /></div>
+      <div class="g"><input id="q" v-model="q"><div id="response" v-if="response">{{response}}</div><ImageGenerator /></div>
       <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
     </ULandingHero><ULandingSection :title="page.features.title" :links="page.features.links"><UPageGrid><ULandingCard v-for="(item,index) of page.features.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
   </div>
@@ -16,14 +16,14 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 import gen from '~/components/gen.vue';
 export default{
   components:{gen},
-  data(){return{q:"",v:null}},
+  data(){return{q:"",response:null}},
   mounted(){setTimeout(()=>{this.snd()},1400)},
   methods:{
     async snd(){
-      const v=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},b:JSON.stringify({ms:document.querySelector('#q').value})});
-      const d=await v.json(); this.v=d.re;
-      //alert("R1: "+this.v);
-      document.querySelector('#t').innerText=this.v;
+      const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},b:JSON.stringify({ms:document.querySelector('#q').value})});
+      const d=await response.json(); this.response=d.re;
+      //alert("R1: "+this.response);
+      document.querySelector('#t').innerText=this.response;
     },
   },
 }
