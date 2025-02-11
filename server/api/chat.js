8a6923371a8fc59e76b756d6777d3a2b759dfb13
Erin Dunn Halligan <erin@designcandy.com>
2025-02-11 08:33:16 -0600

    Update chat.js

--- server/api/chat.js
+++ server/api/chat.js
@@ -1,12 +1,12 @@
-export default defineEventHandler(async(e)=>{
-  const b=await readBody(e);
+export default defineEventHandler(async(event)=>{
+  const body=await readBody(event);
   const ar=await fetch("https://api.openai.com/v1/chat/completions",{
     method:"POST",
     headers:{
       "Content-Type":"application/json",
       Authorization:`Bearer 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`,
     },
-    b:JSON.stringify({model:"gpt-3.5-turbo",messages:[{role:"user",co:b.ms}]}),
+    body:JSON.stringify({model:"gpt-3.5-turbo",messages:[{role:"user",content:body.message}]}),
   });
-  const d=await ar.json(); return{re:d.choices[0].ms.co};
+  const data=await ar.json(); return{reply:data.choices[0].message.content};
 });
