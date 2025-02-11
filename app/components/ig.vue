ac6a3164cd416b9d58eacd9d3811cd080182c8e8
Erin Dunn Halligan <erin@designcandy.com>
2025-02-11 07:17:46 -0600

    Update and rename ImageGenerator.vue to ig.vue

---
+++ app/components/ig.vue
@@ -0,0 +1,16 @@
+<template><div v-if="im"><img id="i" :src="im" /></div></template>
+
+<script>
+export default{
+  mounted(){setTimeout(()=>{this.gen()},1600)},
+  setup(){const q=ref(''); const im=ref('');
+    const gen=async()=>{im.value='';
+      const v=await fetch('https://api.openai.com/v1/images/generations',{method:'POST',headers:{'Content-Type':'application/json',Authorization:`Bearer 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`},body:JSON.stringify({q:document.querySelector('#q').value,n:1,size:'256x256'})});
+      const d=await v.json();
+      //alert("R2: "+JSON.stringify(d));
+      //:if(d&&d.data&&d.data.length>0){im.value=d.data[0].url; document.querySelector('#v').firstChild.style.backgroundImage="url("+im.value+")";}
+    }
+    return{q,im,gen};
+  },
+}
+</script>
