a5e45d7252d20abe92d080c6139971ddeabd7411
Erin Dunn Halligan <erin@designcandy.com>
2025-02-10 21:32:25 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,6 +6,7 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 <template>
   <div>
     <ULandingHero v-if="page.hero" v-bind="page.hero">
+      <img id="ii" src="https://pinfluents.com/_BCK/4/im/green.jpg">
       <div class="g"><input id="prompt" v-model="prompt"><div id="hh" v-if="response">{{response}}</div><ImageGenerator /></div>
       <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
     </ULandingHero><ULandingSection :title="page.features.title" :links="page.features.links"><UPageGrid><ULandingCard v-for="(item,index) of page.features.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
@@ -13,19 +14,35 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 </template>
 
 <script lang="ts">
-import ImageGenerator from '~/components/ImageGenerator.vue';
-export default{
-  components:{ImageGenerator},
-  data(){return{prompt:"",response:null}},
-  mounted(){setTimeout(()=>{this.sendMessage()},1200)},
-  methods:{
-    async sendMessage(){
-      ////alert("PR: "+document.querySelector('#prompt').value);
-      const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector('#prompt').value})});
-      const data=await response.json(); this.response=data.reply;
-      //alert("RES1: "+this.response);
-      document.querySelector('#t').innerText=this.response;
-    },
-  },
+function imageToBase64(ii){
+  const ca=document.createElement("canvas");
+  ca.width=ii.width; ca.height=ii.height;
+  const cx=ca.getContext("2d"); cx.drawImage(ii,0,0);
+  return ca.toDataURL();
 }
+var f=document.querySelector("#ii");
+const r=new FileReader();
+r.onload=function(e){
+  const im=new Image();
+  const img=document.querySelector("ii");
+  const bs=imageToBase64(img); im.src=bs;
+  im.onload=function(){
+    const ca=document.createElement("canvas");
+    ca.width=im.width; ca.height=im.height;
+    const cx=ca.getContext("2d");
+    cx.drawImage(im,0,0);
+    const o=cx.getImageData(0,0,ca.width,ca.height);
+    const d=o.data; const cc={}; let mc=0; let dc="";
+    for(let i=0;i<d.length;i+=4){
+      const r=d[i]; const g=d[i+1]; const b=d[i+2];
+      const rgb=`${r},${g},${b}`;
+      if(cc[rgb]){cc[rgb]++}else{cc[rgb]=1}
+      if(cc[rgb]>mc){mc=cc[rgb]; dc=rgb}
+    }
+    document.body.style.backgroundColor=`rgb(${dc})`;
+  }
+}
+const j=JSON.stringify(f);
+f=new Blob([j],{type:"application/image"});
+r.readAsDataURL(f);
 </script>
