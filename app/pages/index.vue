13377b9a039dc1acb94512358745718a75e18459
Erin Dunn Halligan <erin@designcandy.com>
2025-02-11 21:44:53 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -14,45 +14,32 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 
 <script lang="ts">
 export default{
-  mounted(){
-    this.loadColor();
-  },
+  mounted(){this.loadColor()},
   methods:{
-    async loadColor(){
-      var f=document.getElementById("ii");
-      var r=new FileReader();
-      r.onload=function(e){
-        var im=new Image();
-        var img=document.getElementById("ii");
-        //var bs=imageToBase64(img); im.src=bs;
+    const imageElement = document.getElementById("ii");
+    const canvas = document.getElementById("ca");
+    const context = canvas.getContext("2d");
 
-          var ca=document.getElementById("ca");
-          ca.width=document.getElementById("ii").width; ca.height=document.getElementById("ii").height;
-          var cx=ca.getContext("2d"); cx.drawImage(document.getElementById("ii"),0,0);
-          const dataURL=ca.toDataURL();
-          alert("D: "+dataURL);
+    async function loadImage(url){
+      const response = await fetch(url); // Fetch the image via the network
+      const blob = await response.blob(); // Convert it to a blob
+      const image = new Image();
+      image.crossOrigin = "anonymous"; // Request CORS permissions
+      image.src = URL.createObjectURL(blob); // Load the image from the blob
+      await new Promise((resolve) => {
+        image.onload = resolve; // Wait for image to load
+      });
+      return image;
+    },
+    async function loadColor() {
+      const image = await loadImage(imageElement.src);
+      canvas.width = image.width;
+      canvas.height = image.height;
+      context.drawImage(image, 0, 0);
+      const imageData = context.getImageData(0, 0, canvas.width, canvas.height).data;
 
-          im.src=dataURL;
-
-        alert("IM: "+im.src);
-
-        im.onload=()=>{
-          alert("IM2: "+im.src);
-
-
-          const o=cx.getImageData(0,0,ca.width,ca.height);
-          const d=o.data; const cc={}; let mc=0; let dc="";
-          for(var i=0;i<d.length;i+=4){
-            const r=d[i]; const g=d[i+1]; const b=d[i+2];
-            const rgb=`${r},${g},${b}`;
-            if(cc[rgb]){cc[rgb]++}else{cc[rgb]=1}
-            if(cc[rgb]>mc){mc=cc[rgb]; dc=rgb}
-          }
-          document.body.style.backgroundColor=`rgb(${dc})`;
-        }
-      }
-      var j=JSON.stringify(f); f=new Blob([j],{type:"application/image"}); r.readAsDataURL(f);
-    }
+    };
+    loadColor();
   }
 }
 </script>
