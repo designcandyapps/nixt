c8741aa28f1df6053932a25e6b0525645ec9989c
Erin Dunn Halligan <erin@designcandy.com>
2025-02-12 09:28:00 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -19,30 +19,35 @@ import gen from '~/components/gen.vue';
 export default{
   components:{gen},
   data(){return{q:"",response:null}},
-  mounted(){setTimeout(()=>{this.snd()},1600)},
+  mounted(){
+    loadColor()
+    setTimeout(()=>{
+      this.snd()
+    },1600)
+  },
   methods:{
     async loadColor(){
-  var f=document.getElementById("ii");
-  var r=new FileReader();
-  r.onload=function(e){
-    f=document.getElementById("ii");
-    var im=new Image(); im.src=f.src; //alert(im.src);
-    im.onload=function(){
-     const ca=document.createElement("canvas");
-     ca.width=im.width; ca.height=im.height;
-     var cx=ca.getContext("2d");cx.drawImage(im,0,0);
-     var o=cx.getImageData(0,0,ca.width,ca.height);
-     var d=o.data; var cc={}; let mc=0; let dc="";
-     for(i=0;i<d.length;i+=4){
-       var r=d[i]; var g=d[i+1]; var b=d[i+2];
-       var rgb=`${r},${g},${b}`;
-       if(cc[rgb]){cc[rgb]++}else{cc[rgb]=1}
-       if(cc[rgb]>mc){mc=cc[rgb]; dc=rgb}
-     }
-     document.body.style.backgroundColor=`rgb(${dc})`;
-    }
-  }
-  var j=JSON.stringify(f); f=new Blob([j],{type:"application/image"}); r.readAsDataURL(f);
+      var f=document.getElementById("ii");
+      var r=new FileReader();
+      r.onload=function(e){
+        f=document.getElementById("ii");
+        var im=new Image(); im.src=f.src; //alert(im.src);
+        im.onload=function(){
+          const ca=document.createElement("canvas");
+          ca.width=im.width; ca.height=im.height;
+          var cx=ca.getContext("2d"); cx.drawImage(im,0,0);
+          var o=cx.getImageData(0,0,ca.width,ca.height);
+          var d=o.data; var cc={}; let mc=0; let dc="";
+          for(i=0;i<d.length;i+=4){
+            var r=d[i]; var g=d[i+1]; var b=d[i+2];
+            var rgb=`${r},${g},${b}`;
+            if(cc[rgb]){cc[rgb]++}else{cc[rgb]=1}
+            if(cc[rgb]>mc){mc=cc[rgb]; dc=rgb}
+          }
+          document.body.style.backgroundColor=`rgb(${dc})`;
+        }
+      }
+      var j=JSON.stringify(f); f=new Blob([j],{type:"application/image"}); r.readAsDataURL(f);
     },
     async snd(){
       const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector('#q').value})});
