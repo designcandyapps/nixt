4faaced3addaf4992535bf582c2ea179d554a27f
Erin Dunn Halligan <erin@designcandy.com>
2025-02-12 11:41:56 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -32,7 +32,7 @@ export default{
         var im=new Image();
         im.src=f.src;
 
-        ca=document.getElementById("ca");
+        ca=document.createElement("ca");
         ca.width=im.width; ca.height=im.height;
         var cx=ca.getContext("2d"); cx.drawImage(im,0,0);
 
@@ -42,7 +42,7 @@ export default{
         im.onload=function(){
           alert("IM2: "+im.src);
 
-          ca=document.getElementById("ca");
+          ca=document.createElement("ca");
           ca.width=im.width; ca.height=im.height;
           var cx=ca.getContext("2d"); cx.drawImage(im,0,0);
 
