133a07928771ed431f3199644d8d5dc6da48d43b
Erin Dunn Halligan <erin@designcandy.com>
2025-02-11 21:03:12 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -17,46 +17,12 @@ export default{
   mounted(){this.loadColor()},
   methods:{
     async loadColor(){
+      alert("Test");
 
-
-    function imageToBase64(ii){
-      var ca=document.createElement("canvas");
-      ca.width=ii.width; ca.height=ii.height;
-      var cx=ca.getContext("2d"); cx.drawImage(ii,0,0);
-      return ca.toDataURL();
-    }
-  
       var f=document.getElementById("ii");
-      var r=new FileReader();
-      r.onload=function(e){
-        var im=new Image();
-        var img=document.getElementById("ii");
-        var bs=imageToBase64(img); im.src=bs;
-        alert("IM: "+im.src);
+      alert("F: "+f);
 
-        im.onload=()=>{
-          alert("IM2: "+im.src);
 
-          var ca=document.getElementById("ca");
-          ca.width=im.width; ca.height=im.height;
-          var cx=ca.getContext("2d");
-          cx.drawImage(im,0,0);
-
-          const dataURL=ca.toDataURL();  alert("D: "+dataURL);
-          im.src=ca.toDataURL(); alert("IMS: "+im.src);
-
-          const o=cx.getImageData(0,0,ca.width,ca.height);
-          const d=o.data; const cc={}; let mc=0; let dc="";
-          for(var i=0;i<d.length;i+=4){
-            const r=d[i]; const g=d[i+1]; const b=d[i+2];
-            const rgb=`${r},${g},${b}`;
-            if(cc[rgb]){cc[rgb]++}else{cc[rgb]=1}
-            if(cc[rgb]>mc){mc=cc[rgb]; dc=rgb}
-          }
-          document.body.style.backgroundColor=`rgb(${dc})`;
-        }
-      }
-      var j=JSON.stringify(f); f=new Blob([j],{type:"application/image"}); r.readAsDataURL(f);
     }
   }
 }
