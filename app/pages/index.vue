7268e6e761251917fbcbd7feb6d2d99d044a0407
Erin Dunn Halligan <erin@designcandy.com>
2025-02-13 17:19:30 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -19,7 +19,6 @@ import gen from '~/components/gen.vue';
 export default{
   components:{gen},
   data(){return{q:"",response:null}},
-  const canvas=ref(null),
   mounted(){
     //this.loadColor()
     //setTimeout(()=>{this.snd()},1600)
