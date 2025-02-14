677383a9373ac94a7ae5358198c031418a59e639
Erin Dunn Halligan <erin@designcandy.com>
2025-02-14 00:22:06 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -23,39 +23,38 @@ export default{
     setTimeout(()=>{this.snd()},1600)
   },
   methods:{
+    function imageToBase64(ii){
+      var ca=document.getElementById("ca");
+      ca.width=ii.width; ca.height=ii.height;
+      var cx=ca.getContext("2d"); cx.drawImage(ii,0,0);
+      return ca.toDataURL();
+    },
     async loadColor(){
       var f=document.getElementById("ii");
       const r=new FileReader();
 
+      r.onload=function(e){
+        const im=new Image();
+        const img = document.getElementById('ii');
+        const base64String = imageToBase64(img);
+        im.src=base64String;
 
-
-    r.onload=function(e){
-      const im=new Image();
-      const img = document.getElementById('ii');
-      const base64String = imageToBase64(img);
-      im.src=base64String;
-
-
-      im.onload=function(){
-        const ca=document.getElementById("ca");
-        const cx=ca.getContext("2d");
-        ca.width=im.width; ca.height=im.height;
-        cx.drawImage(im,0,0);
-        const o=cx.getImageData(0,0,ca.width,ca.height);
-        const d=o.data; const cc={}; let mc=0; let dc="";
-        for(let i=0;i<d.length;i+=4){
-          const r=d[i]; const g=d[i+1]; const b=d[i+2];
-          const rgb=`${r},${g},${b}`;
-          if(cc[rgb]){cc[rgb]++}else{cc[rgb]=1}
-          if(cc[rgb]>mc){mc=cc[rgb]; dc=rgb}
+        im.onload=function(){
+          const ca=document.getElementById("ca");
+          const cx=ca.getContext("2d");
+          ca.width=im.width; ca.height=im.height;
+          cx.drawImage(im,0,0);
+          const o=cx.getImageData(0,0,ca.width,ca.height);
+          const d=o.data; const cc={}; let mc=0; let dc="";
+          for(let i=0;i<d.length;i+=4){
+            const r=d[i]; const g=d[i+1]; const b=d[i+2];
+            const rgb=`${r},${g},${b}`;
+            if(cc[rgb]){cc[rgb]++}else{cc[rgb]=1}
+            if(cc[rgb]>mc){mc=cc[rgb]; dc=rgb}
+          }
+          document.body.style.backgroundColor=`rgb(${dc})`;
         }
-        document.body.style.backgroundColor=`rgb(${dc})`;
       }
-      
-
-    }
-
-
       const jsonString = JSON.stringify(f);
       f = new Blob([jsonString], { type: "application/image" });
       console.log(typeof f);
