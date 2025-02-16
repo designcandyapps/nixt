2462fa027c2bf2e75f649bb91a77b10e92ab7238
Erin Dunn Halligan <erin@designcandy.com>
2025-02-16 16:24:19 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -13,15 +13,13 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 </template>
 
 <script lang="ts">
+import {converter,differenceEuclidean,formatHex,nearest} from "https://cdn.skypack.dev/culori@2.0.0";
+import ColorThief from "https://cdn.skypack.dev/colorthief";
+const colorThief=new ColorThief(); const toLCH=converter("lch");
 export default{
   //data(){return{q:"",response:null}},
   mounted(){this.generatePalette()},
   methods:{
-
-    import {converter,differenceEuclidean,formatHex,nearest} from "https://cdn.skypack.dev/culori@2.0.0";
-    import ColorThief from "https://cdn.skypack.dev/colorthief";
-    const colorThief=new ColorThief(); const toLCH=converter("lch");
-
     async adjustHue(val){
       if(val<0)val+=Math.ceil(-val/360)*360;
       return val%360
