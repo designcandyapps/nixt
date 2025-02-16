10c0994240292086766164279d5e55608e1bbe19
Erin Dunn Halligan <erin@designcandy.com>
2025-02-16 16:46:00 -0600

    Create colorthief.vue

---
+++ app/components/colorthief.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { rgb, formatHex } from "culori";
+
+const color = rgb("red"); // Convert color to RGB
+const hexColor = formatHex(color); // Convert to hex format
+</script>
+
+<template>
+  <div>
+    <p>Generated Color: <span :style="{ color: hexColor }">{{ hexColor }}</span></p>
+  </div>
+</template>
