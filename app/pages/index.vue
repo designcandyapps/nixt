f8d8a47417a89150df72467d855775931709290a
Erin Dunn Halligan <erin@designcandy.com>
2025-02-16 16:22:12 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -18,9 +18,9 @@ export default{
   mounted(){this.generatePalette()},
   methods:{
 
-    //import {converter,differenceEuclidean,formatHex,nearest} from "https://cdn.skypack.dev/culori@2.0.0";
-    //import ColorThief from "https://cdn.skypack.dev/colorthief";
-    //const colorThief=new ColorThief(); const toLCH=converter("lch");
+    import {converter,differenceEuclidean,formatHex,nearest} from "https://cdn.skypack.dev/culori@2.0.0";
+    import ColorThief from "https://cdn.skypack.dev/colorthief";
+    const colorThief=new ColorThief(); const toLCH=converter("lch");
 
     async adjustHue(val){
       if(val<0)val+=Math.ceil(-val/360)*360;
