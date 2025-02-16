19dd40943008e1af3b555f2b42cc2cfa2cac7b13
Erin Dunn Halligan <erin@designcandy.com>
2025-02-16 15:29:53 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -23,10 +23,10 @@ export default{
   },
   methods:{
 
-async adjustHue(val){
-  //if(val<0)val+=Math.ceil(-val/360)*360;
-  //return val%360
-},
+    async adjustHue(val){
+      //if(val<0)val+=Math.ceil(-val/360)*360;
+      //return val%360
+    },
 
 
   },
