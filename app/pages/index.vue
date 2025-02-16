1f5f95bb2c7bd91ebe7bd0ca5da23ca74e58882c
Erin Dunn Halligan <erin@designcandy.com>
2025-02-16 15:38:56 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,8 +6,8 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 <template>
   <div>
     <ULandingHero v-if="page.hero" v-bind="page.hero">
-
-      <template #title> <MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
+      
+      <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
     </ULandingHero><ULandingSection :title="page.features.title" :links="page.features.links"><UPageGrid><ULandingCard v-for="(item,index) of page.features.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
   </div>
 </template>
