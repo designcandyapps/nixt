34aede40f97e4ff259d5144f47270ec8b9637f58
Erin Dunn Halligan <erin@designcandy.com>
2025-02-16 17:02:58 -0600

    Update culori.js

--- app/plugins/culori.js
+++ app/plugins/culori.js
@@ -1,3 +1,7 @@
+import {converter,differenceEuclidean,formatHex,nearest} from "https://cdn.skypack.dev/culori@2.0.0";
+import ColorThief from "https://cdn.skypack.dev/colorthief";
+const colorThief=new ColorThief(); const toLCH=converter("lch");
+
 import { defineNuxtPlugin } from "#app";
 import * as culori from "culori";
 
