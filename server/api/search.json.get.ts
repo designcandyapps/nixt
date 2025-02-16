2ea916921acc982bbccc09a26f26f08ddf81bdc6
Erin Dunn Halligan <erin@designcandy.com>
2025-02-16 16:33:40 -0600

    Update search.json.get.ts

--- server/api/search.json.get.ts
+++ server/api/search.json.get.ts
@@ -1,3 +1,6 @@
+import {converter,differenceEuclidean,formatHex,nearest} from "https://cdn.skypack.dev/culori@2.0.0";
+import ColorThief from "https://cdn.skypack.dev/colorthief";
+const colorThief=new ColorThief(); const toLCH=converter("lch");
 import {serverQueryContent} from '#content/server'
 export default eventHandler(async(event)=>{
   return serverQueryContent(event).where({_type:'markdown',navigation:{$ne:false}}).find()
