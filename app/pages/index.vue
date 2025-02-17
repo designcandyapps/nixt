e66ecbb20263fa8528516927137b987e26432ec6
Erin Dunn Halligan <erin@designcandy.com>
2025-02-17 07:04:47 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -20,7 +20,7 @@ export default{
     this.generatePalette()
   },
   methods:{
-    const img=document.getElementById("ii"); alert("IM: "+img); //const img=document.createElement("img");
+    var img=document.getElementById("ii"); alert("IM: "+img); //var img=document.createElement("img");
     //img.src=u; alert("IM2: "+img.src); //img.crossOrigin=`anonymous`; await img.decode(); return img;
     async adjustHue(val){if(val<0)val+=Math.ceil(-val/360)*360;return val%360},
     async createScientificPalettes(baseColor){
