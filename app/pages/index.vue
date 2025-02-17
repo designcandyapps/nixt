14aa0484341b4b0f4a565d9f28b0e751401468f2
Erin Dunn Halligan <erin@designcandy.com>
2025-02-17 10:03:24 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -52,7 +52,7 @@ export default{
       let colors=[]; let chosenImg; let img; const queries=["red","green","blue","yellow","orange","magenta","pink","purple","turqoise","grey","black","white","indigo","violet","emerald","flower","vibrant","gold","silver","jewels","rainbow","forest","ocean","coral","galaxy","tree","leaf","fish","frog","animal","wildlife","color","paint","paint","abstract","colorful","nature","volcano","sun","ruby","saphire","emerald",""];
       while(colors.length<4){
         //const u=`https://images.unsplash.com/photo-1732279446743-324499ebbeba?w=800&amp;auto=format&amp;fit=crop&amp;q=60&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0NHx8fGVufDB8fHx8fA%3D%3D`;
-        const u=`https://pinfluents.com/_BCK/4/im/bg.jpg`;
+        const u=`https://pinfluents.com/_BCK/4/im/bp.png`;
         //chosenImg=await loadImg(u);
         img=document.getElementById("ii");
         img.src=u; img.crossOrigin=`anonymous`; img=img.decode(); chosenImg=img;
