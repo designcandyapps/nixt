5029c6b1d8a67b1cd5ad2ba314f100f2d283051e
Erin Dunn Halligan <erin@designcandy.com>
2025-02-17 09:33:36 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,8 +7,7 @@ import {rgb,formatHex} from "culori"; const color=rgb("red"); const hexColor=for
 <template>
   <div>
     <ULandingHero v-if="page.hero" v-bind="page.hero">
-      <img id="ii" src="https://pinfluents.com/_BCK/4/im/dc2.png" style="position:relative; width:200px; height:120px; border:2px solid blue;">
-      <div id="z" style="position:relative; width:200px; height:50px; border:2px solid red;"></div>
+      <img id="ii" src="https://pinfluents.com/_BCK/4/im/dc2.png"><div id="z" style="border:2px solid red;"></div>
       <!--template><div style="position:relative; width:200px; height:50px; border:2px solid green;">Color: <span :style="{color:hexColor}">{{hexColor}}</span></div></template-->
       <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
     </ULandingHero><ULandingSection :title="page.features.title" :links="page.features.links"><UPageGrid><ULandingCard v-for="(item,index) of page.features.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
