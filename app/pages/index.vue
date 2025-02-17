bf78ce056c76473a625aecba0e478bf241b7b06d
Erin Dunn Halligan <erin@designcandy.com>
2025-02-17 09:06:17 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -53,10 +53,11 @@ export default{
       while(colors.length<4){
         const u=`https://images.unsplash.com/photo-1732279446743-324499ebbeba?w=800&amp;auto=format&amp;fit=crop&amp;q=60&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0NHx8fGVufDB8fHx8fA%3D%3D`;//https://designcandy.com/im/lo.png
         //chosenImg=await loadImg(u);
-
-        img=document.getElementById("ii"); img.src=u; img.crossOrigin=`anonymous`; img=img.decode();
+        //img=document.createElement("img");
+        img=document.getElementById("ii");
+        img.src=u; img.crossOrigin=`anonymous`; img=img.decode();
         chosenImg=img;
-        alert("IMS: "+img.src); alert("CHO: "+chosenImg);
+        alert("CHO: "+chosenImg); alert("IMS: "+chosenImg.src);
 
 
         colors=await colorThief.getPalette(chosenImg).map((c)=>toLCH({r:c[0]/255,g:c[1]/255,b:c[2]/255,mode:"rgb"}));
