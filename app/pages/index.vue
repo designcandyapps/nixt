768f595ec623f5a28011a5b674cc15ecbfff6366
Erin Dunn Halligan <erin@designcandy.com>
2025-02-17 09:48:26 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -43,9 +43,8 @@ export default{
       return palettes;
     },
     async loadImg(u){
-      let img;
-      //const img=document.createElement("img");
-      img=document.getElementById("ii");
+      const img=document.createElement("img");
+      //const img=document.getElementById("ii");
       img.src=u; img.crossOrigin=`anonymous`;
       await img.decode(); return img;
     },
