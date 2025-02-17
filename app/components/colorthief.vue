3cedd0173ae7a80f4310e2da39358fb557f570d1
Erin Dunn Halligan <erin@designcandy.com>
2025-02-16 19:31:42 -0600

    Update colorthief.vue

--- app/components/colorthief.vue
+++ app/components/colorthief.vue
@@ -8,6 +8,7 @@ export default{
     const colorThief=new ColorThief(); const toLCH=converter("lch");
     //const colorthief=async()=>{
 
+alert("Test");
 /*
     async adjustHue(val){
       if(val<0)val+=Math.ceil(-val/360)*360;
