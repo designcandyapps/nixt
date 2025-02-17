96044e7f7d95b5a9f6cdc5f3a78d2b8d56dc8716
Erin Dunn Halligan <erin@designcandy.com>
2025-02-17 15:21:44 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,4 @@
+//--INDEX.VUE:
 <script setup lang="ts">
 const {data:page}=await useAsyncData('index',()=>queryContent('/').findOne())
 useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,description:page.value.description,ogDescription:page.value.description})
@@ -8,7 +9,6 @@ import {rgb,formatHex} from "culori"; const color=rgb("red"); const hexColor=for
   <div>
     <ULandingHero v-if="page.hero" v-bind="page.hero">
       <img id="ii" src="https://pinfluents.com/_BCK/4/im/dc2.png"><div id="z" style="border:2px solid red;"></div>
-      <div id="co" class="content" style="border:5px solid orange;"></div>
       <!--template><div style="position:relative; width:200px; height:50px; border:2px solid green;">Color: <span :style="{color:hexColor}">{{hexColor}}</span></div></template-->
       <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
     </ULandingHero><ULandingSection :title="page.features.title" :links="page.features.links"><UPageGrid><ULandingCard v-for="(item,index) of page.features.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
@@ -27,7 +27,7 @@ export default{
     },
     async isColorEqual(c1,c2){return c1.h===c2.h && c1.l===c2.l && c1.c===c2.c},
     async discoverPalettes(colors){
-      //alert("Tist");
+      alert("Tist");
       const palettes={};
       for(const color of colors){
         const targetPalettes=createScientificPalettes(color);
@@ -44,49 +44,36 @@ export default{
       return palettes;
     },
     async loadImg(u){
+      let img;
       //const img=document.createElement("img");
-      const img=document.getElementById("ii");
+      img=document.getElementById("ii");
       img.src=u; img.crossOrigin=`anonymous`;
       await img.decode(); return img;
     },
     async generatePalette(){
       let colors=[]; let chosenImg; let img; const queries=["red","green","blue","yellow","orange","magenta","pink","purple","turqoise","grey","black","white","indigo","violet","emerald","flower","vibrant","gold","silver","jewels","rainbow","forest","ocean","coral","galaxy","tree","leaf","fish","frog","animal","wildlife","color","paint","paint","abstract","colorful","nature","volcano","sun","ruby","saphire","emerald",""];
       while(colors.length<4){
-        const u=`https://images.unsplash.com/photo-1732279446743-324499ebbeba?w=800&amp;auto=format&amp;fit=crop&amp;q=60&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0NHx8fGVufDB8fHx8fA%3D%3D`;
-        ////const u=`https://pinfluents.com/_BCK/4/im/bp.png`;
+        const u=`https://images.unsplash.com/photo-1732279446743-324499ebbeba?w=800&amp;auto=format&amp;fit=crop&amp;q=60&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0NHx8fGVufDB8fHx8fA%3D%3D`;//https://designcandy.com/im/lo.png
         //chosenImg=await loadImg(u);
-        
+
+        //img=document.createElement("img");
         img=document.getElementById("ii");
-        img.crossOrigin=`anonymous`;
-        img.src=u;
-        img=img.decode();
-        
-        chosenImg=img;
-        //chosenImg=await img.decode();
-        alert("CH: "+chosenImg); alert("CI: "+chosenImg.src);
-        alert("IM: "+img); alert("IMS: "+img.src);
+        img.src=u; img.crossOrigin=`anonymous`; img=img.decode(); chosenImg=img;
+        alert("CH: "+chosenImg); alert("IM: "+im.src);
         colors=await colorThief.getPalette(chosenImg).map((c)=>toLCH({r:c[0]/255,g:c[1]/255,b:c[2]/255,mode:"rgb"}));
       }
       const palettes=discoverPalettes(colors);
 
-      ////document.body.innerHTML=`<div id="co" class="content" style="border:2px solid yellow;"></div>`;
-      //document.getElementById("z").innerHTML=`<div id="co" class="content" style="border:2px solid pink;"></div>`;
-      alert("Z: "+document.querySelector("#z"));
-      //alert("ZC: "+document.querySelector("#z").innerHTML);
+      //document.body.innerHTML=`<div id="co" class="content" style="border:2px solid yellow;"></div>`;
+      document.getElementById("z").innerHTML=`<div id="co" class="content" style="border:2px solid pink;"></div>`;
+      //alert("Z: "+document.querySelector("#z"));
       //alert("CO: "+document.querySelector(".content"));
       //alert("CH2: "+chosenImg);
       document.body.appendChild(chosenImg); //img
 
       for(const type of Object.keys(palettes)){
-        const paletteWrapper=document.createElement("div");
-        ////const paletteWrapper=document.getElementById("z");
-        paletteWrapper.style.border="border:10px solid green";
-        
-        paletteWrapper.classList.add("palette-colors");
-        document.querySelector(".content").appendChild(paletteWrapper);
-        //document.querySelector("#z").appendChild(paletteWrapper); paletteWrapper.innerHTML=`<p style="border:2px solid aqua;">${type}</p>`;
-
-        paletteWrapper.innerHTML=`<p style="border:2px solid aqua;">${type}</p>`;
+        const paletteWrapper=document.createElement("div"); paletteWrapper.classList.add("palette-colors");
+        document.querySelector(".content").appendChild(paletteWrapper); paletteWrapper.innerHTML=`<p style="border:2px solid aqua;">${type}</p>`;
         paletteWrapper.innerHTML+=palettes[type].colors.reduce((html,color)=>{html+=`<div style="border:2px solid lavender; background:${formatHex(color)};"></div>`;return html},"");
       }
       setTimeout(()=>{generatePalette()},1000);
@@ -110,9 +97,9 @@ img {
   height:100vh;
   object-fit:cover;
   filter:brightness(.75);
-  -border:5px solid blue;
+  border:5px solid pink;
 }
-.content,#z {
+.content {
   display:grid;
   grid-gap:1rem;
   padding:2rem;
@@ -127,5 +114,4 @@ img {
   -border:1px solid hsla(0,0%,100%,.9);
   border:5px solid purple;
 }
-#z { border:5px solid green; }
 </style>
