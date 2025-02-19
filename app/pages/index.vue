3a77855a9060229cff69fee98a7c96608d341cce
Erin Dunn Halligan <erin@designcandy.com>
2025-02-19 05:39:53 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,6 +6,7 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 <template>
   <div>
     <ULandingHero v-if="page.hero" v-bind="page.hero">
+      <NuxtImg ref="image" src="https://pinfluents.com/_BCK/4/im/dc2.png" width="60" height="60" provider="sirv" @load="getCP" /><canvas ref="canvas" width="60" height="60"></canvas>
       <div class="g"><input id="q" v-model="q"><div id="response" v-if="response">{{response}}</div><ImageGenerator /></div>
       <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
     </ULandingHero><ULandingSection :title="page.features.title" :links="page.features.links"><UPageGrid><ULandingCard v-for="(item,index) of page.features.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
@@ -17,7 +18,15 @@ import gen from '~/components/gen.vue';
 export default{
   components:{gen},
   data(){return{q:"",response:null}},
-  mounted(){setTimeout(()=>{this.snd()},1600)},
+  mounted(){
+    const imE=this.$refs.image.$el.querySelector("img");
+    imE.onload=()=>{
+      const canvas=this.$refs.canvas;
+      const cx=canvas.getContext("2d");
+      cx.drawImage(imE,0,0,canvas.width,canvas.height);
+    },
+    setTimeout(()=>{this.snd()},1600)
+  },
   methods:{
     async snd(){
       const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector('#q').value})});
