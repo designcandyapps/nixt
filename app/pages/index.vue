ac99b81272f25c78551f4d35f34c9b51a72752c1
Erin Dunn Halligan <erin@designcandy.com>
2025-02-20 05:30:54 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,7 +6,7 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 <template>
   <div id="ff">
     <ULandingHero v-if="page.hero" v-bind="page.hero">
-      <img id="xx" src="https://pinfluents.com/_BCK/4/im/lo.png"><canvas id="ca" ref="canvas" width="60" height="60"></canvas>
+      <!--img id="xx" src="https://pinfluents.com/_BCK/4/im/lo.png"--><canvas id="ca" ref="canvas" width="60" height="60"></canvas>
       <div class="g"><input id="q" v-model="q"><div id="response" v-if="response">{{response}}</div><ImageGenerator /></div>
       <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
     </ULandingHero><ULandingSection :title="page.features.title" :links="page.features.links"><UPageGrid><ULandingCard v-for="(item,index) of page.features.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
