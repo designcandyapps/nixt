191eb5350d9535a5356aec28091b55dd666141d9
Erin Dunn Halligan <erin@designcandy.com>
2025-02-23 10:54:05 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -19,7 +19,7 @@ export default{
   components:{gen},
   data(){return{q:"",response:null}},
   mounted(){
-    const ca=document.getElementById("ca"); const cx=ca.getContext("2d"); cx.fillStyle="red"; cx.fillRect(10,10,100,100); //alert("CA0: "+ca); 
+    //const ca=document.getElementById("ca"); const cx=ca.getContext("2d"); cx.fillStyle="red"; cx.fillRect(10,10,100,100); //alert("CA0: "+ca); 
     //this.getCP();
     //setTimeout(()=>{this.snd()},1600);
   },
