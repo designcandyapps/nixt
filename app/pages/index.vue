48b47b208a2882b19cb64e3e74ee6810dd99c4d8
Erin Dunn Halligan <erin@designcandy.com>
2025-02-23 15:13:05 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -24,7 +24,7 @@ export default{
     //cvx.fillStyle="red"; cvx.fillRect(10,10,100,100);
 
     const r=new FileReader(); r.onload=function(){
-    const e=document.querySelector("#iz");
+    const e=document.querySelector("#i1 img");
     alert("IM: "+e.src);
 
       const im=new Image();
