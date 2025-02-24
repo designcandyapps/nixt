4986c1784abd0c8fc827e031ae09d401643be4c9
Erin Dunn Halligan <erin@designcandy.com>
2025-02-24 11:05:40 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -21,8 +21,8 @@ export default{
     async function bs64(t){//alert("zT: "+t.src);
       if(!t){return} var cv=document.getElementById("cv");
       cv.width=t.width; cv.height=t.height; var cx=cv.getContext("2d");
-      cx.drawImage(t,0,0);
-      //cx.fillStyle="red"; cx.fillRect(0,0,cv.width,cv.height);
+      cx.fillStyle="red"; cx.fillRect(0,0,cv.width,cv.height);
+      cx.drawImage(t,0,0,cv.width,cv.height);
       var o=cx.getImageData(0,0,cv.width,cv.height);
       var d=o.data; var cc={}; let mc=0; let dc="";
       for(let i=0;i<d.length;i+=4){var r=d[i];var g=d[i+1];var b=d[i+2];var rgb=`${r},${g},${b}`;if(cc[rgb]){cc[rgb]++}else{cc[rgb]=1}if(cc[rgb]>mc){mc=cc[rgb];dc=rgb}}
