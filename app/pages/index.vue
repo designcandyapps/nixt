d50e2bfe9222f983d4afa047cbe3dbc799658b4b
Erin Dunn Halligan <erin@designcandy.com>
2025-02-26 13:55:45 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -23,11 +23,10 @@ export default{
       if(!t){return} var cv=document.getElementById("cv");
       cv.width=t.width; cv.height=t.height; var cx=cv.getContext("2d");
       cx.fillStyle="red"; cx.fillRect(0,0,cv.width,cv.height);
-      var o=cx.getImageData(0,0,cv.width,cv.height);
-      var d=o.data; var cc={}; let mc=0; let dc="";
-      for(let i=0;i<d.length;i+=4){var r=d[i];var g=d[i+1];var b=d[i+2];var rgb=`${r},${g},${b}`;if(cc[rgb]){cc[rgb]++}else{cc[rgb]=1}if(cc[rgb]>mc){mc=cc[rgb];dc=rgb}}
-      const du=cv.toDataURL(); return du;
-      alert("DU: "+du);
+      //var o=cx.getImageData(0,0,cv.width,cv.height);
+      //var d=o.data; var cc={}; let mc=0; let dc="";
+      //for(let i=0;i<d.length;i+=4){var r=d[i];var g=d[i+1];var b=d[i+2];var rgb=`${r},${g},${b}`;if(cc[rgb]){cc[rgb]++}else{cc[rgb]=1}if(cc[rgb]>mc){mc=cc[rgb];dc=rgb}}
+      const du=cv.toDataURL(); return du; alert("DU: "+du);
       return cv.toDataURL();
     }
     async function bs64rgb(b64,callback){
