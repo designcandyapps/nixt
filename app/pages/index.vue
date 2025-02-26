9482b18e42901e227731d798f2f0c6e445e828c9
Erin Dunn Halligan <erin@designcandy.com>
2025-02-26 14:15:17 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -19,14 +19,10 @@ export default{
   data(){return{q:"",response:null}},
   mounted(){
     var f=document.querySelector("#ee");
-    async function bs64(t){alert("T: "+t);
+    async function bs64(t){//alert("T: "+t);
       if(!t){return} var cv=document.getElementById("cv");
       cv.width=t.width; cv.height=t.height; var cx=cv.getContext("2d");
       cx.fillStyle="red"; cx.fillRect(0,0,cv.width,cv.height);
-      //var o=cx.getImageData(0,0,cv.width,cv.height);
-      //var d=o.data; var cc={}; let mc=0; let dc="";
-      //for(let i=0;i<d.length;i+=4){var r=d[i];var g=d[i+1];var b=d[i+2];var rgb=`${r},${g},${b}`;if(cc[rgb]){cc[rgb]++}else{cc[rgb]=1}if(cc[rgb]>mc){mc=cc[rgb];dc=rgb}}
-      const du=cv.toDataURL(); return du; alert("DU: "+du);
       return cv.toDataURL();
     }
     async function bs64rgb(b64,callback){
