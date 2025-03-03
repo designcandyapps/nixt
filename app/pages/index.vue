89fb428f9534defe01261d1c7dde07c5954de4ae
Erin Dunn Halligan <erin@designcandy.com>
2025-03-02 18:54:48 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,7 +6,7 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 <template>
   <div>
     <ULandingHero v-if="page.hero" v-bind="page.hero">
-      <div class="g"><input id="q" v-model="q"><div id="response" v-if="response">{{response}}</div><ImageGenerator /></div>
+      <div class="g"><input id="prompt" v-model="prompt"><div id="hh" v-if="response">{{response}}</div><ImageGenerator /></div>
       <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
     </ULandingHero><ULandingSection :title="page.features.title" :links="page.features.links"><UPageGrid><ULandingCard v-for="(item,index) of page.features.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
   </div>
@@ -16,11 +16,11 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 import ImageGenerator from '~/components/ImageGenerator.vue';
 export default{
   components:{ImageGenerator},
-  data(){return{q:"",response:null}},
+  data(){return{prompt:"",response:null}},
   mounted(){setTimeout(()=>{this.sendMessage()},1400)},
   methods:{
     async sendMessage(){
-      const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector('#q').value})});
+      const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector('#prompt').value})});
       const data=await response.json(); this.response=data.reply;
       //alert("RES1: "+this.response);
       document.querySelector('#t').innerText=this.response;
