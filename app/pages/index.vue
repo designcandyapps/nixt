5b54da43f790614c527f208467b951381828ec70
Erin Dunn Halligan <erin@designcandy.com>
2025-03-02 18:32:51 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,24 +6,25 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 <template>
   <div>
     <ULandingHero v-if="page.hero" v-bind="page.hero">
-      <div class="g"><input id="q" v-model="q"><div id="response" v-if="response">{{response}}</div><ImageGenerator /></div>
+      <div class="g"><input id="prompt" v-model="prompt"><div id="hh" v-if="response">{{response}}</div><ImageGenerator /></div>
       <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
     </ULandingHero><ULandingSection :title="page.features.title" :links="page.features.links"><UPageGrid><ULandingCard v-for="(item,index) of page.features.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
   </div>
 </template>
 
 <script lang="ts">
-import gen from '~/components/gen.vue';
+import ImageGenerator from '~/components/ImageGenerator.vue';
 export default{
-  components:{gen},
-  data(){return{q:"",response:null}},
-  mounted(){setTimeout(()=>{this.snd()},1600)},
+  components:{ImageGenerator},
+  data(){return{prompt:"",response:null}},
+  mounted(){setTimeout(()=>{this.sendMessage()},1400)},
   methods:{
-    async snd(){
-      const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector('#q').value})});
+    async sendMessage(){
+      const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector('#prompt').value})});
       const data=await response.json(); this.response=data.reply;
-      //alert("R1: "+this.response);
+      //alert("RES1: "+this.response);
       document.querySelector('#t').innerText=this.response;
+      //alert("T: "+document.querySelector('#t').innerText);
     },
   },
 }
