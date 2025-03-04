6fb26cc90dc39cd00bd4a3728f668c95aee84f30
Erin Dunn Halligan <erin@designcandy.com>
2025-03-04 10:26:36 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,7 +6,7 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 <template>
   <div>
     <ULandingHero v-if="page.hero" v-bind="page.hero">
-      <div class="g"><input id="prompt" v-model="prompt"><div id="response" v-if="response">{{response}}</div><ImageGenerator />
+      <div class="g"><input id="prompt" v-model="prompt"><div id="response" v-if="response">{{response}}</div><!--ImageGenerator /-->
         <div id="dr"></div><canvas id="cv"></canvas><canvas id="cvB"></canvas>
       </div>
       <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
@@ -15,9 +15,7 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 </template>
 
 <script lang="ts">
-//import gen from '~/components/gen.vue';
 export default{
-  //components:{gen},
   data(){return{prompt:"",response:null}},
   mounted(){
     var f=document.querySelector("#ee");
