8f47e87bb0caca8693d22eceb17c3505dc967c2d
Erin Dunn Halligan <erin@designcandy.com>
2025-03-10 10:22:08 -0500

    Create prox.js

---
+++ server/api/prox.js
@@ -0,0 +1,16 @@
+export default defineEventHandler(async(event)=>{
+  const body=await readBody(event);
+  const apiResponse=await fetch("/",{
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json",
+      "Access-Control-Allow-Origin":"*",
+      "Cache-Control":"no-cache"
+    },
+    body:JSON.stringify({
+      messages:[{content:body.message}],
+    }),
+  });
+  const data=await apiResponse.json();
+  return{reply:data.choices[0].message.content};
+});
