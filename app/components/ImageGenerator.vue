6ccb460031189c95ea721ba4f3efa7dc231aecb1
Erin Dunn Halligan <erin@designcandy.com>
2025-03-11 01:50:57 -0500

    Update ImageGenerator.vue

--- app/components/ImageGenerator.vue
+++ app/components/ImageGenerator.vue
@@ -4,7 +4,7 @@ export default{
   mounted(){setTimeout(()=>{this.generateImage()},1600)},
   setup(){const prompt=ref(''); const image=ref('');
     const generateImage=async()=>{image.value='';
-      const response=await fetch('https://api.openai.com/v1/images/generations',{method:'POST',headers:{'Content-Type':'application/json',Authorization:`Bearer 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`},body:JSON.stringify({prompt:document.querySelector('#prompt').value,n:1,size:'256x256'})});
+      const response=await fetch('https://api.openai.com/v1/images/generations',{method:'POST',headers:{'Content-Type':'application/json',Authorization:`Bearer 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`},body:JSON.stringify({prompt:document.querySelector('#prompt').value,n:1,size:'256x256'})});
       const data=await response.json();
       //alert("RES2: "+JSON.stringify(data));
       if(data&&data.data&&data.data.length>0){image.value=data.data[0].url; document.querySelector('#v').firstChild.style.backgroundImage="url("+image.value+")";}
