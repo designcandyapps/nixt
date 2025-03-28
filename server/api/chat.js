bbc2207db8064225973fc26c4dae38766f721a02
Erin Dunn Halligan <erin@designcandy.com>
2025-03-28 10:29:20 -0500

    Update chat.js

--- server/api/chat.js
+++ server/api/chat.js
@@ -4,7 +4,7 @@ export default defineEventHandler(async(event)=>{
     method:"POST",
     headers:{
       "Content-Type": "application/json",
-      Authorization: `Bearer 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`;
+      Authorization: `Bearer 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`,
       //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`,
     },
     body:JSON.stringify({
