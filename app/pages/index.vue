8334089179494d58da74f91543a3580cb5488502
Erin Dunn Halligan <erin@designcandy.com>
2025-03-29 11:43:36 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -28,7 +28,7 @@ export default{
     async function fetchGI(query,apiKey,page=1,pageSize=1){
       alert("Q: "+query);
       const url=`https://api.gettyimages.com/v3/search/images?phrase=${encodeURIComponent(query)}&page=${page}&page_size=${pageSize}`;
-      //try{
+      try{
         const response=await fetch(url,{headers:{"Api-Key":apiKey}});
         //alert("RES: "+JSON.stringify(response));
         //if(!response.ok){throw new Error(`Err: ${response.status}-${response.statusText}`)}
@@ -39,7 +39,7 @@ export default{
           thumbUrl:image.display_sizes[0]?.uri||'',
           previewUrl:image.display_sizes[1]?.uri||''
         }));
-      //}catch(error){console.error("Err: ",error); return []}
+      }catch(error){console.error("Err: ",error); return []}
     }
     //setTimeout(()=>{this.snd()},2200);
     setTimeout(()=>{
