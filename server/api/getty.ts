2d2528df353ed3dc3a2ea30932487223f6951bac
Erin Dunn Halligan <erin@designcandy.com>
2025-03-31 01:51:30 -0500

    Update getty.ts

--- server/api/getty.ts
+++ server/api/getty.ts
@@ -1,35 +1,16 @@
 export default defineEventHandler(async(event)=>{
+  const query=getQuery(event);
+  alert(query);
 
-    const apiKey="lep3mq3jxr4u99m7hy3gzzp3gl";
-    const zquery=prompt.value; //document.querySelector("#prompt").value;
-    async function fetchGI(zquery,apiKey,page=1,pageSize=1){
+  //https://api.gettyimages.com/v3/search/images?phrase=sunset&page_size=1
+  const imageUrl=query.url as string;
+  alert(imageUrl);
 
-      const url=`https://api.gettyimages.com/v3/search/images?phrase=${encodeURIComponent(zquery)}&page=${page}&page_size=${pageSize}`;
-      try{
-        const response=await fetch(url,{headers:{"Api-Key":apiKey}});
-        alert("RES0: "+this.response);
-        if(!response.ok){throw new Error(`Error: ${response.status}-${response.statusText}`)}
-        const data=await response.json();
-        alert("RES1: "+JSON.stringify(data));
-        return data.images.map(image=>({
-          id:image.id,title:image.title||'',
-          thumbUrl:image.display_sizes[0]?.uri||'',
-          previewUrl:image.display_sizes[1]?.uri||''
-        }));
-      }catch(error){console.error("Error2: ",error); return []}
-    }
-
-
-
-
-
-  
-  const query=getQuery(event); const imageUrl=query.url as string;
   if(!imageUrl){return new Response("Invalid URL",{status:400})}
   try{const response=await fetch(imageUrl,{headers:{"User-Agent":"Mozilla/5.0"}});
   if(!response.ok){return new Response("Error",{status:500})}
   const contentType=response.headers.get("Content-Type");
-  if(!contentType||!contentType.startsWith("image/")){return new Response("Not Img URL",{status:400})}
+  if(!contentType||!contentType.startsWith("image/")){return new Response("Not URL",{status:400})}
   return new Response(await response.arrayBuffer(),{headers:{"Content-Type":contentType,"Access-Control-Allow-Origin":"*","Cache-Control":"no-cache"}})}
   catch(error){return new Response("Server Error",{status:500})}
 });
