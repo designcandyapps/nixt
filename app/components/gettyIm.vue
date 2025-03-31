9f6317a393ed5974ab4dd33c6ede2cb9e2da2f40
Erin Dunn Halligan <erin@designcandy.com>
2025-03-31 02:44:54 -0500

    Update gettyIm.vue

--- app/components/gettyIm.vue
+++ app/components/gettyIm.vue
@@ -4,11 +4,12 @@ const fetchGI=async(query)=>{
   const apiUrl=`https://api.gettyimages.com/v3/search/images`;
   try{
     const response=await fetch(`${apiUrl}?phrase=${encodeURIComponent(query)}&page_size=1`,{method:"GET",headers:{"Api-Key":"lep3mq3jxr4u99m7hy3gzzp3gl"}});
-    if(!response.ok){throw new Error(`Error1: ${response.statusText}`)}
+    //throw new Error(`Error1:${response.statusText}`);
+    //if(!response.ok){throw new Error(`Error1:${response.statusText}`)}
     const data=await response.json(); this.response=data.reply;
     alert("RES1: "+this.response);
     if(data.images&&data.images.length>0){const image=data.images[0];alert("Im: "+image);return image}else{console.log("No ims");return null}
-  }catch(error){console.error("Error2: ",error)}
+  }catch(error){console.error("Error2:",error)}
 
   //isLoading.value=true;
 //proxyUrl.value=`https://api.gettyimages.com/v3/search/images?phrase=${encodeURIComponent(query)}`; //`/api/getty?phrase=${encodeURIComponent(query)}`;
