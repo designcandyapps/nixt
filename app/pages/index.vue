c7a73f68148c2721aedff65989158d00c20ba2d6
Erin Dunn Halligan <erin@designcandy.com>
2025-03-31 02:55:05 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,7 +6,7 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 <template>
   <div>
     <ULandingHero v-if="page.hero" v-bind="page.hero">
-      <gettyGen />
+      <gettyIm />
       <div id="pv"><input id="pho" v-model="pho"></div><div id="z"><ColorThief /></div>
       <div class="g"><input id="prompt" v-model="prompt"><div id="response" v-if="response">{{response}}</div><!--ImageGenerator /--></div>
       <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
@@ -28,16 +28,16 @@ export default{
       const apiUrl=`https://api.gettyimages.com/v3/search/images`;
       try{
         const response=await fetch(`${apiUrl}?phrase=${encodeURIComponent(query)}&page_size=1`,{method:"GET",headers:{"Api-Key":apiKey}});
-        if(!response.ok){throw new Error(`Error: ${response.statusText}`)}
+        if(!response.ok){throw new Error(`Error1:${response.statusText}`)}
         const data=await response.json();
-        if(data.images&&data.images.length>0){const image=data.images[0]; console.log("Im:",image); return image}else{console.log("No ims"); return null}
-      }catch(error){console.error("Error:",error)}
+        if(data.images&&data.images.length>0){const image=data.images[0];console.log("Im:",image);return image}else{console.log("No ims");return null}
+      }catch(error){console.error("Error2:",error)}
     }
     fetchGI("sunset").then(image=>{
-      if(image){
+      //if(image){
         alert("IM: "+image.display_sizes[0].uri);
         //pho.value=query; //document.querySelector("#a").style.backgroundImage="url("+pho.value+")";
-      }
+      //}
     });*/
     //setTimeout(()=>{this.snd()},2200);
   },
