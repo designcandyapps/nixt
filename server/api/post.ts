fd5219913501ffb46d4f7a373acc275bfcff10d0
Erin Dunn Halligan <erin@designcandy.com>
2025-03-31 03:45:41 -0500

    Update post.ts

--- server/api/post.ts
+++ server/api/post.ts
@@ -2,7 +2,7 @@ export default defineEventHandler(async(event)=>{
   const query=getQuery(event);
   const body=await readBody(event);
   const apiResponse=await fetch("https://api.gettyimages.com/v3/search/images?phrase=${encodeURIComponent(query)}",{
-    method:"GET",
+    method:"POST",
     headers:{
       "Content-Type": "application/json",
       "Api-Key":`lep3mq3jxr4u99m7hy3gzzp3gl`,
