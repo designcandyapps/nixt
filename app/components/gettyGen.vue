70daf0d2c077714380d719319915495c3d949f18
Erin Dunn Halligan <erin@designcandy.com>
2025-03-31 02:22:08 -0500

    Create gettyGen.vue

---
+++ app/components/gettyGen.vue
@@ -0,0 +1,20 @@
+<template><div v-if="image"><img id="i" :src="image" /></div></template>
+<script>
+export default{
+  mounted(){setTimeout(()=>{this.gettyGen()},1600)},
+  setup(){const prompt=ref(""); const image=ref("");
+    const gettyGen=async()=>{image.value="";
+      const response=await fetch("https://api.gettyimages.com/v3/search/images?phrase=radiant",{method:"GET",headers:{
+        "Content-Type":"application/json",
+        Authorization:`lep3mq3jxr4u99m7hy3gzzp3gl`,
+        "Api-Key":`lep3mq3jxr4u99m7hy3gzzp3gl`
+      },
+      body:JSON.stringify({prompt:"radial",n:1,size:"256x256"})});
+      const data=await response.json();
+      alert("RES2: "+JSON.stringify(data));
+      if(data&&data.data&&data.data.length>0){image.value=data.data[0].url; document.querySelector("#v").firstChild.style.backgroundImage="url("+image.value+")";}
+    }
+    return{prompt,image,gettyGen};
+  },
+}
+</script>
