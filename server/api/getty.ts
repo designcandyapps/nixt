d3439af091e6e672c08257585757bdbc63584660
Erin Dunn Halligan <erin@designcandy.com>
2025-03-31 02:07:23 -0500

    Update getty.ts

--- server/api/getty.ts
+++ server/api/getty.ts
@@ -2,15 +2,19 @@ export default defineEventHandler(async(event)=>{
   const query=getQuery(event);
   alert(query);
 
-  //const imageUrl=`https://api.gettyimages.com/v3/search/images?phrase=sunset&page_size=1`;
-  const imageUrl=query.phrase as string;
+  const imageUrl=query.phrase as string; //`https://api.gettyimages.com/v3/search/images?phrase=sunset&page_size=1`;
   alert(imageUrl);
 
   if(!imageUrl){return new Response("Invalid URL",{status:400})}
-  try{const response=await fetch(imageUrl,{headers:{"User-Agent":"Mozilla/5.0"}});
-  if(!response.ok){return new Response("Error",{status:500})}
-  const contentType=response.headers.get("Content-Type");
-  if(!contentType||!contentType.startsWith("image/")){return new Response("Not URL",{status:400})}
-  return new Response(await response.arrayBuffer(),{headers:{"Content-Type":contentType,"Access-Control-Allow-Origin":"*","Cache-Control":"no-cache"}})}
+  try{
+    const response=await fetch(imageUrl,{
+      headers:{
+        "User-Agent":"Mozilla/5.0",
+      }
+    });
+    if(!response.ok){return new Response("Error",{status:500})}
+    const contentType=response.headers.get("Content-Type");
+    if(!contentType||!contentType.startsWith("image/")){return new Response("Not URL",{status:400})}
+    return new Response(await response.arrayBuffer(),{headers:{"Content-Type":contentType,"Access-Control-Allow-Origin":"*","Cache-Control":"no-cache"}})}
   catch(error){return new Response("Server Error",{status:500})}
 });
