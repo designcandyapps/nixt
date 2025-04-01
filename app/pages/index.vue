86bce198822f21e73771090029061724d43e2c23
Erin Dunn Halligan <erin@designcandy.com>
2025-04-01 08:13:14 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,8 +6,6 @@ useSeoMeta({titleTemplate:'',title:page.value.title,ogTitle:page.value.title,des
 <template>
   <div>
     <ULandingHero v-if="page.hero" v-bind="page.hero">
-      <div id="z"><ColorThief /></div>
-      <div class="g"><input id="prompt" v-model="prompt"><div id="response" v-if="response">{{response}}</div></div>
       <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
     </ULandingHero>
     <ULandingSection :title="page.features.title" :links="page.features.links"><UPageGrid><ULandingCard v-for="(item,index) of page.features.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
