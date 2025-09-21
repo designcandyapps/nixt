5348ab29f9fc7c60fe7404b9ce9b33a21db57f58
Erin Dunn Halligan <erin@designcandy.com>
2025-09-20 21:36:48 -0500

    Update Slider.vue

--- app/components/Slider.vue
+++ app/components/Slider.vue
@@ -1,22 +1,8 @@
 <template>
-  <span>
-  <span id="ns" class="news-slider">
-    <span>
-      <h2>Room to grow</h2>
-      <p>Scale as you go with unmetered bandwidth on all plans. Zero downtime. No limits.</p>
-    </span>
-    <span>
-      <h2>Seriously secure</h2>
-      <p>Firewalls, encryption, brute force, and DDoS protection. Security’s all taken care of.</p>
-    </span>
-    <span>
-      <h2>Bring it with you</h2>
-      <p>Fast data transfer and performance come as standard with our global CDN—wherever your site visitors are.</p>
-    </span>
-    <span>
-      <h2>Flexible storage</h2>
-      <p>You’ll never run out of storage with WordPress.com’s plans. Photos. Audio. 4K videos. All in one place.</p>
-    </span>
-  </span>
-  </span>
+  <div class="news-slider">
+    <div><p></p></div>
+    <div><p></p></div>
+    <div><p></p></div>
+    <div><p></p></div>
+  </div>
 </template>
